6e5f4af56f151add26376508c738d937325dc147
Sibtain28 <you@example.comsibtainahmedqureshi28@gmail.com>
2025-04-05 17:04:32 +0530

    UI Done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,35 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+      <LinkColumn>
+      <LinkTitle>Call</LinkTitle>
+      <LinkItem href="tel:6264963698">6264963698</LinkItem>
+      </LinkColumn>
+
+      <LinkColumn>
+      <LinkTitle>Email</LinkTitle>
+      <LinkItem href="mailto:sibtainahmedqureshi28@gmail.com">sibtainahmedqureshi28@gmail.com</LinkItem>
+      </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Made with ❤️ by Sibtain </Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+        <SocialIcons href="https://github.com/Sibtain28">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.linkedin.com/in/sibtain-ahmed-qureshi/">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="https://www.instagram.com/sibtain_28/">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      </SocialContainer>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
